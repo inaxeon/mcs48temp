af9206c35df9f277b2be34bee9626ea1e4d6cef9
Matt <Matt@D1NC01>
2020-11-01 08:12:01 +0000

    Resolved merge conflict

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
 
-C:\dev\asm48-win32\asm48.exe main.asm && C:\dev\hvepromcmd\x64\Debug\HvEpromCmd.exe -p COM2 -d 8742 -o write -f main.bin -v -b
+C:\dev\asm48-win32\asm48.exe main.asm && C:\dev\hvepromcmd\x64\Debug\HvEpromCmd.exe -p COM1 -d 8742 -o write -f main.bin -v -b
